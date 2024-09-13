4635d71fb4fe439fadf4a8b79686534f427c14ef
JasonLewis12 <Lewisjason64@yahoo.com>
2024-09-13 18:02:41 -0400

    added logic to app.jsx

--- puppypal/src/App.jsx
+++ puppypal/src/App.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { puppyList } from "./data";
 
-function App() {}
+function App() {
+  const [puppies, setpuppies] = useState(puppyList);
+
+  return (
+    <>
+      <div>
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
