b16f48876db6e092ca0a995945af263c1a11258c
JasonLewis12 <Lewisjason64@yahoo.com>
2024-09-13 19:33:15 -0400

    completed homewoek

--- puppypal/src/App.jsx
+++ puppypal/src/App.jsx
@@ -6,13 +6,36 @@ import { puppyList } from "./data";
 
 function App() {
   const [puppies, setpuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   return (
     <>
       <div>
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
-        })}
+          return (
+            <p
+              className="card"
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}{" "}
+        <p>
+          {featPupId && (
+            <div className="card">
+              <h2>{featuredPup.name}</h2>
+              <ul>
+                <li>Age: {featuredPup.age}</li>
+                <li>Email: {featuredPup.email}</li>
+              </ul>
+            </div>
+          )}
+        </p>
       </div>
     </>
   );
